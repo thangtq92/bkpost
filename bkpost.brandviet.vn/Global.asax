93895aca49d61dd65f20623ddb4d2870e7688f2e
thangtq92 <thangtq92@gmail.com>
2015-11-06 07:51:40 +0700

    Delete folder bkpost.brandviet.vn

--- bkpost.brandviet.vn/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NB_Commerce.MvcApplication" Language="C#" %>
--- bkpost.brandviet.vn/Scripts/main/app.js
+++
@@ -1,7 +0,0 @@
-﻿HREF = window.location.hostname; // Assign href auto:
-PORT = ':' + location.port;
-Languages = {};
-AppConfigs = {
-    SECRET_KEY: '26C2E7C8-C689-D1D5-C452-58EC5A2F2A39',
-    HOST: 'http://' + HREF + PORT,
-};
